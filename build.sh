19e9f3ef81725e589197240155543b0b3ebe3008
noroi-turtle <chelonii672@gmail.com>
2021-06-28 19:58:06 +0700

    change order of -lsfml-* because compiler will crash

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir -p out/
-g++ src/main.cpp -o out/exporter -O2 -std=c++2a -lsfml-system -lsfml-graphics -I lib/spine/include lib/spine/src/spine/*.cpp
+g++ src/main.cpp -o out/exporter -O2 -std=c++2a -I lib/spine/include lib/spine/src/spine/*.cpp -lsfml-system -lsfml-graphics
